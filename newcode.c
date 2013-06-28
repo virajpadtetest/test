18dce75c52559156f7b5119ea4a6701f00f5b1da
virajpadtetest <viraj@brainchamber.net>
2013-06-28 16:47:10 +0530

    Create newcode.c
    Created new code. Added simple adding functionality in the same.

---
+++ newcode.c
@@ -0,0 +1,9 @@
+int main(){
+  int a,b,v;
+  printf("\nEnter the value of an integer\n");
+  a = scanf("%d");
+  b =10;
+  v =a+b;
+  printf("The result of the operation is : %d",v);
+  return 0;
+}
